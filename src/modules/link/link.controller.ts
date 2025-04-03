0a06d1edb05af48d7356e8da2a583bdd479d61c4
mammaadDeveloper <developermammaad@gmail.com>
2025-04-03 21:25:31 +0330

    feat(contoller, service, package, environment): Link shortener implementation.
    Implement link shortener. Install crypto package. Add controller method

--- src/modules/link/link.controller.ts
+++ src/modules/link/link.controller.ts
@@ -1,12 +1,46 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Res } from '@nestjs/common';
 import { LinkService } from './link.service';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
+interface BaseResponse<T> {
+  success: boolean,
+  status: number,
+  message: string,
+  data?: T
+};
 @Controller('link')
 export class LinkController {
-  constructor(private readonly linkService: LinkService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly linkService: LinkService,
+  ) {}
 
   @Post()
-  async createShortLink(@Body('url') url: string){
-    
+  @HttpCode(HttpStatus.CREATED)
+  async createShortLink(@Body('url') url: string): Promise<BaseResponse<{link: string}>>{
+    const link = await this.linkService.createShortLink(url);
+    return {
+      success: true,
+      status: HttpStatus.CREATED,
+      message: 'Operation successful!',
+      data: {
+        link: `${this.configService.get('app.url')}/${link.shortCode}`
+      }
+    };
+  }
+
+  @Get(':code')
+  async getOriginalUrl(@Param('code') code: string, @Res() res: Response): Promise<Response | void>{
+    const link = await this.linkService.getOriginalUrl(code);
+
+    if(link)
+      return res.redirect(link);
+
+    return res.status(HttpStatus.NOT_FOUND).json({
+      success: false,
+      status: HttpStatus.NOT_FOUND,
+      message: 'Link not found!'
+    });
   }
 }
--- src/modules/link/link.service.ts
+++ src/modules/link/link.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { LinkEntity } from './entities/link.entity';
 import { Repository } from 'typeorm';
+import * as crypto from 'crypto';
 
 @Injectable()
 export class LinkService {
@@ -9,7 +10,15 @@ export class LinkService {
         @InjectRepository(LinkEntity)
         private readonly linkRepository: Repository<LinkEntity>
     ){}
-  async createShortLink(url: string){
-    
+  async createShortLink(url: string): Promise<LinkEntity>{
+    const shortCode = crypto.randomBytes(3).toString('hex');
+    const shortLink = this.linkRepository.create({originalUrl: url, shortCode});
+    await this.linkRepository.save(shortLink);
+    return shortLink;
+  }
+
+  async getOriginalUrl(shortCode: string): Promise<string | null>{
+    const link = await this.linkRepository.findOne({where: {shortCode}});
+    return link? link.originalUrl: null;
   }
 }
