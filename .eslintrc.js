9e46e2bd51089ad0237c123dc6a2fdeeabd10f32
mammaadDeveloper <developermammaad@gmail.com>
2025-04-03 20:19:11 +0330

    perf: Initial project preparations.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "prettier:prettier/recommended"
   ],
   root: true,
   env: {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger();
+  app.useLogger(logger);
   await app.listen(process.env.PORT ?? 3000);
+  const url = await app.getUrl();
+  logger.log(`Application running on port ${url}`);
 }
 bootstrap();
